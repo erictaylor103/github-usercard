c139164aebba97c98974a5e44fa2ec33097eca70
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-09 19:17:54 -0400

    Commiting the firstpart of the project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,25 +1,33 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+
+//axios.get("https://api.github.com/users/erictaylor103")
+//.then(res =>{
+//  console.log(res.data);
+//})
+
+
+
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
@@ -29,13 +37,13 @@ const followersArray = [];
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
-<div class="card">
+<div class="card">--container
   <img src={image url of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -46,10 +54,86 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
+function createCard(object){
+  //CREATING THE ELEMENTS FOR THE HTML PAGE
+  const container = document.createElement("div");
+  const image     = document.createElement("img");
+  const info      = document.createElement("div");
+  const name      = document.createElement("h3");
+  const username  = document.createElement("p");
+  const location  = document.createElement("p");
+  const profile   = document.createElement("p");
+  const anchor    = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio       = document.createElement("p");
+
+  //STRUCTURE
+  container.append(image);
+  container.append(info);
+  info.append(name);
+  info.append(username);
+  info.append(location);
+  info.append(profile);
+  info.append(followers);
+  info.append(following);
+  info.append(bio);
+  profile.appendChild(anchor);
+
+  //CREATE THE CLASSES
+  container.classList.add("card");
+  info.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  //SET THE TEXT CONTENT, DATA AND IMAGE URL
+  image.src             = object.avatar_url;
+  name.textContent      = "Name: " + object.name;
+  username.textContent  = "Username: " + object.login;
+  location.textContent  = "Location: " + object.location;
+  profile.href          = "Profile: " + object.url;
+  profile.textContent   = object.url;
+  followers.textContent = "Followers: " + object.followers;
+  following.textContent = "Following: " + object.following;
+  bio.textContent       = "Bio: " + object.bio;
+
+  return container;
+
+
+}
+//createCard(); //I USED THIS TO BE ABLE TO CONSOLE LOG ITEMS
+
+
+
+
+
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
   luishrd
   bigknell
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//END OF FILE
